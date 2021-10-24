6327b06b2655155a4743464c7bbba7a65bd95f2d
Clare Cruz <cbrightly1@gmail.com>
2021-10-24 18:52:14 -0400

    small fix in q1

--- Homework 5/q1/q1.py
+++ Homework 5/q1/q1.py
@@ -1,14 +1,12 @@
 # Q1
-
 def custom_transpose(input_matrix):
 	n = len(input_matrix)
 	m = len(input_matrix[0])
 	new = [[0 for i in range(n)] for j in range(m)]
 	for i in range(n):
 		for j in range(m):
-			new[j][i] = input_matrix[i][j]
+			new[j][i] = input_matrix[i][j]	
 	return new
 
 output = custom_transpose(([[10,8],[2,4],[1,7]]))
-for i in range(len(output)):
-	print(output[i])
+print(*output, sep = "\n")
